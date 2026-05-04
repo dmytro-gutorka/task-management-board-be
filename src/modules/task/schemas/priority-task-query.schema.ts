81f3b7bf1a6f35a9d4941a633a1b0934603f6b34
Dmytro Gutorka <dgutorka@gmail.com>
2026-05-04 18:33:59 +0300

    refactor: add defaults to schemas

--- src/modules/task/schemas/priority-task-query.schema.ts
+++ src/modules/task/schemas/priority-task-query.schema.ts
@@ -4,5 +4,7 @@ import { TaskPriorityFilter } from '../enums/task-priority.enum.js';
 const priorityValues = Object.values(TaskPriorityFilter);
 
 export const priorityTaskQuerySchema = z.object({
-  priority: z.enum(priorityValues, `Available priority fields are: ${priorityValues.join(', ')}`),
+  priority: z
+    .enum(priorityValues, `Available priority fields are: ${priorityValues.join(', ')}`)
+    .default(TaskPriorityFilter.ALL),
 });
--- src/modules/task/schemas/status-task-query.schema.ts
+++ src/modules/task/schemas/status-task-query.schema.ts
@@ -4,5 +4,7 @@ import { TaskFilterStatus } from '../enums/task-status.enum.js';
 const statusValues = Object.values(TaskFilterStatus);
 
 export const statusTaskQuerySchema = z.object({
-  status: z.enum(statusValues, `Available status fields are: ${statusValues.join(', ')}`),
+  status: z
+    .enum(statusValues, `Available status fields are: ${statusValues.join(', ')}`)
+    .default(TaskFilterStatus.ALL),
 });
