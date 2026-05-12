dffce5929f037496dc29223a89ae5a92e3efd86b
Dmytro Gutorka <93080155+dmytro-gutorka@users.noreply.github.com>
2026-05-12 18:36:48 +0300

    chore: add user-preferences table

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -2,10 +2,13 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToOne,
   PrimaryGeneratedColumn,
+  type Relation,
   UpdateDateColumn,
 } from 'typeorm';
 import type { Nullable } from '@types';
+import { UserPreferencesEntity } from '../../user-preferences/entities/user-preferences.entity.js';
 
 @Entity('users', { orderBy: { id: 'ASC' } })
 export class UserEntity {
@@ -23,6 +26,19 @@ export class UserEntity {
 
   @Column({ nullable: true, type: 'date' })
   birthday: Nullable<Date>;
+
+  @Column({ name: 'avatar_url', type: 'varchar', length: 255, nullable: true })
+  avatarUrl: Nullable<string>;
+
+  @Column({
+    name: 'last_login_at',
+    type: 'timestamptz',
+    nullable: true,
+  })
+  lastLoginAt: Nullable<Date>;
+
+  @OneToOne(() => UserPreferencesEntity, (preferences) => preferences.user)
+  preferences: Relation<UserPreferencesEntity>;
 
   @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
   createdAt: Date;
