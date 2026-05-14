595d8d1b4791dc52276cceaa671ef198f811f461
Dmytro Gutorka <dgutorka@gmail.com>
2026-05-14 13:01:49 +0300

    refactor: fix imports

--- src/modules/notification/index.ts
+++ src/modules/notification/index.ts
@@ -1,4 +1,5 @@
-export { EmailOutboxEntity, EmailOutboxStatus } from './entities/email-outbox.entity.js';
+export { EmailOutboxStatus } from './notofication.enums.js';
+export { EmailOutboxEntity } from './entities/email-outbox.entity.js';
 export { runNotificationModuleComposer } from './notification-module.composer.js';
 export { EmailOutboxService } from './services/email-outbox.service.js';
-export type { CreateEmailOutboxInput, UpdateEmailOutboxInput } from './types.js';
+export type { CreateEmailOutboxInput, UpdateEmailOutboxInput } from './notification.types.js';
